850089c389fea933bda5098aedd3d7f16b2496ae
Cole Pergerson <cole.pergerson@gmail.com>
2022-02-04 01:09:53 -0800

    Player will now "attack" enemies when clicked on.

--- Dying MMO Submission/Assets/Scripts/AgentMoveToTarget.cs
+++ Dying MMO Submission/Assets/Scripts/AgentMoveToTarget.cs
@@ -7,13 +7,88 @@ using UnityEngine.AI;
 public class AgentMoveToTarget : MonoBehaviour
 {
     NavMeshAgent navAgent;
+
+    public delegate void DestinationCompleted();
+
     private void Awake()
     {
         navAgent = GetComponent<NavMeshAgent>();
     }
 
-    public void SetDestination(Vector3 postion)
+    public void SetDestination(Vector3 destination)
     {
-        navAgent.destination = postion;
+        navAgent.destination = destination;
+        navAgent.stoppingDistance = 0;
+    }
+
+    public void SetDestination(Vector3 destination, float stoppingDistance)
+    {
+        navAgent.destination = destination;
+        navAgent.stoppingDistance = stoppingDistance;
+    }
+
+    /// <param name="destination">Position on the floor.</param>
+    /// <param name="OnDestinationCompleted">Callback function when agent reaches the destination.</param>
+    public void SetDestination(Vector3 destination, float stoppingDistance, DestinationCompleted OnDestinationCompleted)
+    {
+        navAgent.destination = destination;
+        navAgent.stoppingDistance = stoppingDistance;
+        StopAllCoroutines();
+        StartCoroutine(CheckForDestinationCompleted(OnDestinationCompleted));
+    }
+
+    IEnumerator CheckForDestinationCompleted(DestinationCompleted OnDestinationCompleted)
+    {
+        var destinationReached = false;
+        var distanceCheckErrors = 0;
+
+        while(!destinationReached)
+        {
+            var remainingDistance = GetPathRemainingDistance(navAgent);
+
+            if (distanceCheckErrors >= 5)
+            {
+                Debug.LogError(gameObject.name + " in AgentMoveToTarget.cs -> CheckForDestinationCompleted(): Too many distanceErrorChecks. " +
+                    "GetPathRemainingDistance is returning -1 too many times, which means there is an issue with the path being created");
+                break;
+            }
+
+            if (remainingDistance < 0)
+            {
+                distanceCheckErrors++;
+            }
+
+
+            if (remainingDistance <= navAgent.stoppingDistance && remainingDistance > 0)
+            {
+                if (OnDestinationCompleted != null)
+                    OnDestinationCompleted();
+                else
+                    Debug.LogError(gameObject.name + " in AgentMoveToTarget.cs -> CheckForDestinationCompleted(): Callback is null");
+
+                destinationReached = true;
+            }
+
+            yield return new WaitForSeconds(.1f);
+        }
+
+
+    }
+
+    // from https://stackoverflow.com/a/61449518
+    public static float GetPathRemainingDistance(NavMeshAgent navMeshAgent)
+    {
+        if (navMeshAgent.pathPending ||
+            navMeshAgent.pathStatus == NavMeshPathStatus.PathInvalid ||
+            navMeshAgent.path.corners.Length == 0)
+            return -1f;
+
+        float distance = 0.0f;
+        for (int i = 0; i < navMeshAgent.path.corners.Length - 1; ++i)
+        {
+            distance += Vector3.Distance(navMeshAgent.path.corners[i], navMeshAgent.path.corners[i + 1]);
+        }
+
+        return distance;
     }
 }
--- Dying MMO Submission/Assets/Scripts/PlayerController.cs
+++ Dying MMO Submission/Assets/Scripts/PlayerController.cs
@@ -9,11 +9,14 @@ public class PlayerController : MonoBehaviour
     private PlayerControls controls;
     private PlayerInput playerInput;
     private AgentMoveToTarget move;
-    [SerializeField] LayerMask layermask;
+    private AgentAttack attack;
+    [SerializeField] LayerMask layerMask;
+
     void Awake()
     {    
         playerInput = GetComponent<PlayerInput>();
         move = GetComponent<AgentMoveToTarget>();
+        attack = GetComponent<AgentAttack>();
         controls = new PlayerControls();
     }
 
@@ -40,9 +43,21 @@ public class PlayerController : MonoBehaviour
         var ray = Camera.main.ScreenPointToRay(mousePosition);
         RaycastHit hit;
 
-        if(Physics.Raycast(ray, out hit))
+        if(Physics.Raycast(ray, out hit, layerMask))
         {
-            move.SetDestination(hit.point);
+
+            if(hit.collider.tag == "Ground")
+            {
+                move.SetDestination(hit.point, 0);
+                
+            } 
+            else if(hit.collider.tag == "Enemy")
+            {
+                AgentMoveToTarget.DestinationCompleted onDestinationCompleted = attack.Attack;
+                var destination = hit.collider.gameObject.transform.position;
+                move.SetDestination(destination, 2f, onDestinationCompleted);
+            }
+
         }
     }
 }
