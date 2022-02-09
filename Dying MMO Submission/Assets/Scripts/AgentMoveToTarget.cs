58513385fc3c334824062ed45ee0909d9ae8a7e1
Cole Pergerson <cole.pergerson@gmail.com>
2022-02-09 14:47:02 -0800

    Created a hub world blockout

--- Dying MMO Submission/Assets/Scripts/AgentMoveToTarget.cs
+++ Dying MMO Submission/Assets/Scripts/AgentMoveToTarget.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-[RequireComponent(typeof(NavMeshAgent), typeof(Agent))]
+[RequireComponent(typeof(NavMeshAgent))]
 public class AgentMoveToTarget : MonoBehaviour
 {
     [Header("Audio")]
