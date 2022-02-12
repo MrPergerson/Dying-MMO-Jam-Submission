d70ba7ff45d39772f694c7f460d1e7cccaf68ac2
Cole Pergerson <cole.pergerson@gmail.com>
2022-02-12 15:20:52 -0800

    Add WhenTimeHasPassed story condition

---
+++ Dying MMO Submission/Assets/Scripts/Story System/StoryConditions/WhenTimeHasPassed.cs
@@ -0,0 +1,37 @@
+using Sirenix.OdinInspector;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhenTimeHasPassed : StoryCondition
+{
+    [SerializeField] private float timeInSecondsToWait = 5;
+    [SerializeField, ReadOnly] private float timeLeft;
+
+    private void OnEnable()
+    {
+        ResetCondition();
+    }
+
+    private void Update()
+    {
+        if(IsCurrentCondition())
+        {
+            timeLeft -= Time.deltaTime;
+        }
+    }
+
+    public override bool IsConditionMet()
+    {
+        if (timeLeft <= 0)
+            conditionMet = true;
+
+        return conditionMet;
+    }
+
+    public override void ResetCondition()
+    {
+        timeLeft = timeInSecondsToWait;
+        conditionMet = false;
+    }
+}
