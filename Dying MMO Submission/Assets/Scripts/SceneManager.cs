4ad5ba49ea82d87defd1e94fd0958f92f223db6e
Cole Pergerson <cole.pergerson@gmail.com>
2022-02-13 23:13:01 -0800

    started scene manager

---
+++ Dying MMO Submission/Assets/Scripts/SceneManager.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using DG.Tweening;
+
+public class SceneManager : MonoBehaviour
+{
+    //public void 
+}
