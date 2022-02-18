4ccfbfd5ad5da24fd735e5729195af6040c27b66
ASokry <anthonysokry11@gmail.com>
2022-02-17 22:57:07 -0800

    New Message Box UI

--- Dying MMO Submission/Assets/Scripts/ScriptableObjects/UIAudioData.cs
+++ Dying MMO Submission/Assets/Scripts/ScriptableObjects/UIAudioData.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 [CreateAssetMenu(fileName = "UIAudioData", menuName = "Data/UIAudioData")]
 public class UIAudioData : ScriptableObject
@@ -9,10 +10,10 @@ public class UIAudioData : ScriptableObject
     [SerializeField] private AudioClip _alert;
     [SerializeField] private AudioClip _chatPing;
     [SerializeField] private List<AudioClip> _buttonAudioSet;
+    [SerializeField] private AudioMixerGroup _audioMixer;
 
     public AudioClip Alert { get { return _alert; } }
     public AudioClip ChatPing { get { return _chatPing; } }
     public List<AudioClip> ButtonAudioSet { get { return _buttonAudioSet; } }
-
-
+    public AudioMixerGroup AudioMixer { get { return _audioMixer; } }
 }
